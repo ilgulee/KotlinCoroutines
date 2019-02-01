aae49f5d53ae82c06c13dbc98af0a1c60c09abf5
Ilgu Lee <iglee2000ca@gmail.com>
2019-02-01 11:33:30 -0500

    main thread starts after runBlocking

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,9 @@
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) {
-    exampleBlocking()
+    exampleBlockingDispatcher()
 }
 
 suspend fun printlnDelayed(message: String) {
@@ -15,4 +16,15 @@ fun exampleBlocking() = runBlocking {
     println("one")
     printlnDelayed("two")
     println("three")
+}
+
+// Running on another thread but still blocking the main thread
+fun exampleBlockingDispatcher() {
+    runBlocking(Dispatchers.Default) {
+        println("one - from thread ${Thread.currentThread().name}")
+        printlnDelayed("two - from thread ${Thread.currentThread().name}")
+    }
+    // Outside of runBlocking to show that it's running in the blocked main thread
+    println("three - from thread ${Thread.currentThread().name}")
+    // It still runs only after the runBlocking is fully executed.
 }
