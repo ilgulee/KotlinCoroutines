67df51049a68b95a4b42e8c85e595f0a7b1551b5
Ilgu Lee <iglee2000ca@gmail.com>
2019-02-01 12:07:38 -0500

    Concurrent using async and await

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,21 @@
+
 import kotlinx.coroutines.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 fun main(args: Array<String>) {
-    exampleLaunchCoroutineScope()
+    exampleAsyncAwait()
 }
 
 suspend fun printlnDelayed(message: String) {
     // Complex calculation
     delay(1000)
     println(message)
+}
+
+suspend fun calculateHardThings(startNum: Int): Int {
+    delay(1000)
+    return startNum * 10
 }
 
 fun exampleBlocking() = runBlocking {
@@ -63,4 +69,18 @@ fun exampleLaunchCoroutineScope() = runBlocking {
     println("three - from thread ${Thread.currentThread().name}")
 
     (customDispatcher.executor as ExecutorService).shutdown()
+}
+
+fun exampleAsyncAwait() = runBlocking {
+    val startTime = System.currentTimeMillis()
+
+    val deferred1 = async { calculateHardThings(10) }
+    val deferred2 = async { calculateHardThings(20) }
+    val deferred3 = async { calculateHardThings(30) }
+
+    val sum = deferred1.await() + deferred2.await() + deferred3.await()
+    println("async/await result = $sum")
+
+    val endTime = System.currentTimeMillis()
+    println("Time taken: ${endTime - startTime}")
 }
